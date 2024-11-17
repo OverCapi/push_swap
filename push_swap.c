68804386d9f33c4849c6ce4869a6a2c7a4780a02
Louis Lemmel <llemmel@k1r2p14.42mulhouse.fr>
2024-11-17 06:34:38 +0100

    Parsing done

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: llemmel <llemmel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 13:27:37 by llemmel           #+#    #+#             */
-/*   Updated: 2024/11/17 06:18:45 by llemmel          ###   ########.fr       */
+/*   Updated: 2024/11/17 06:33:29 by llemmel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,20 +42,8 @@ int	main(int argc, char **argv)
 		return (0);
 	init_stack(&stack_a);
 	init_stack(&stack_b);
-	if (argc == 3)
-	{
-		if (argv && argv[1])
-		{
-			argv_split = ft_split(argv[1], ' ');
-			if (!argv_split)
-				return ((void)write(2, ERROR_MSG, ERROR_MSG_LEN), 1);
-			if (parse_number_split(argv_split, &stack_a) != 0)
-				return ((void)write(2, ERROR_MSG, ERROR_MSG_LEN), 1);
-		}
-	}
-	else
-		if (parse_number_n(argv, argc, &stack_a) != 0)
-			return ((void)write(2, ERROR_MSG, ERROR_MSG_LEN), 1);
+	if (parse_number_n(argv, argc, &stack_a) != 0)
+		return ((void)write(2, ERROR_MSG, ERROR_MSG_LEN), 1);
 	print_lst(stack_a);
 	ft_lstclear_ps(&stack_a.list, NULL);
 	return (0);
